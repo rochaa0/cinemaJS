bf97a8e4105ce1ed0c8eb0f4c02562a8e067109b
rochaa0 <gustavorocha2004@outlook.com>
2023-05-23 20:20:26 -0300

    cinema js

---
+++ app/Http/Controllers/filmeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Http\Request;
+use App\Models\Filme;
+
+class filmeController extends Controller
+{
+    //construimos o CRUD aqui
+
+    public function buscaCadastroFilme(){
+        return View('cadastroFilme');
+    }
+
+    public function cadastrarFilme(Request $request){
+        $dadosFilme = $request->validate([
+            'nomefilme' => 'string|required',
+            'atoresfilme' => 'string|required',
+            'datalancamentofilme' => 'string|required',
+            'sinopsefilme' => 'string|required',
+            'capafilme' => 'file|required'
+        ]);
+       // dd($dadosFilme);
+
+        $file = $dadosFilme['capafilme'];
+        $path = $file->store('capa','public');
+        $dadosFilme['capafilme'] = $path;
+        
+        Filme::create($dadosFilme);
+
+        //return Redirect::route('/home');
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\filmeController;
+use App\Http\Controllers\funcionarioController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/adm', function () {
+    return view('home');
+})->name('home');
+
+Route::get('/', function () {
+    return view('site/index');
+})->name('index');
+
+Route::get('/login', function () {
+    return view('site/login');
+})->name('login');
+
+Route::get('/cadastro-filme',[filmeController::class,'buscaCadastroFilme'])->name('buscar-cadastro-filme');
+Route::post('/cadastro-filme',[filmeController::class,'cadastrarFilme'])->name('cadastro-filme');
+
+Route::get('/cadastro-funcionario',[funcionarioController::class,'buscarCadastroFuncionario'])->name('buscar-cadastro-funcionario');
+Route::post('/cadastro-funcionario',[funcionarioController::class,'cadastrarFuncionario'])->name('cadastro-funcionario');
+Route::get('/gerenciar-funcionario',[funcionarioController::class,'MostrarGerenciadorFuncionario'])->name('gerenciar-funcionario');
+
+Route::delete('/gerenciar-funcionario/{registrosFuncionarios}',[funcionarioController::class,'ApagarBancoFuncionario'])->name('apagar-funcionario');
+
+Route::get('/alterar-funcionario/{registroFuncionario}',[funcionarioController::class,'MostrarRegistrosFuncionario'])->name('mostrar-funcionario');
+Route::put('/gerenciar-funcionario/{registrosFuncionarios}',[funcionarioController::class,'AlterarBancoFuncionario'])->name('alterar-banco-funcionario');
